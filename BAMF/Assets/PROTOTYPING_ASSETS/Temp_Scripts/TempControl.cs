3f592c8d229845424b0d99af3be0e765b740f897
danielfisher3 <38573896+danielfisher3@users.noreply.github.com>
2018-12-19 18:14:07 -0600

    Added a small prototype
    Added temp walls for  cemetary, Knight,and temp Control

---
+++ BAMF/Assets/PROTOTYPING_ASSETS/Temp_Scripts/TempControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempControl : MonoBehaviour {
+
+    [SerializeField] float moveSpeed = 10, rotSpeed = 1;
+    float hMove;
+    float vertMove;
+	void Start () {
+		
+	}
+	
+	
+	void LateUpdate () {
+        hMove = Input.GetAxis("Horizontal");
+        if(Input.GetButton("Horizontal") && hMove > 0)
+        {
+            transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+        }
+        else if(Input.GetButton("Horizontal") && hMove < 0)
+        {
+            transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
+        }
+
+
+        vertMove = Input.GetAxis("Vertical");
+        if(Input.GetButton("Vertical") && vertMove > 0)
+        {
+            transform.Translate( 0, 0, moveSpeed * Time.deltaTime);
+        }
+        else if(Input.GetButton("Vertical") && vertMove < 0)
+        {
+            transform.Translate(0, 0, -moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.R)){
+            transform.Rotate(0, rotSpeed, 0);
+        }
+       
+	}
+}
