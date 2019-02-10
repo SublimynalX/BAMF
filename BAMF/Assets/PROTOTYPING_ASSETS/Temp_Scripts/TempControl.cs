365f34477eec704c3aa98c18f881ea25fc6161ee
Daniel Fisher <danielefisher3@gmail.com>
2019-02-10 15:40:48 -0600

    Player
    Added Barbarian with temp rig and temp anims

--- BAMF/Assets/PROTOTYPING_ASSETS/Temp_Scripts/TempControl.cs
+++ BAMF/Assets/PROTOTYPING_ASSETS/Temp_Scripts/TempControl.cs
@@ -9,22 +9,25 @@ public class TempControl : MonoBehaviour {
     [SerializeField] float speed = 5f;
     float gravity = 9.81f;
 
-
+    Animator playerAnim;
 
     
 	void Start () {
         cControl = GetComponent<CharacterController>();
+        playerAnim = GetComponentInChildren<Animator>();
 	}
 	
 	
 	void Update () {
         CalculateMovement();
+
        
 	}
     void CalculateMovement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalMovement = Input.GetAxis("Vertical");
+        playerAnim.SetFloat("Walk", verticalMovement);
         Vector3 direction = new Vector3(horizontalInput, 0, verticalMovement);
         Vector3 velocity = direction * speed;
         velocity.y -= gravity;
